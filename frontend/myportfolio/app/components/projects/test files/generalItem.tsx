82f692516da5a0e3fad22f734b15060bc94be881
Linus L <linlo001@student.liu.se>
2024-12-14 15:37:14 +0100

    progress with carousel animation

--- frontend/myportfolio/app/components/projects/test files/generalItem.tsx
+++ frontend/myportfolio/app/components/projects/test files/generalItem.tsx
@@ -139,38 +139,38 @@ const GeneralItem = () => {
   console.log((dim.x * factor) / 100);
   return (
     <div id="HAAAHAAA" className="bg-slate-400 h-[500px] w-[500px] relative py-7 px-7 ">
-      {dim && 
-         <svg
-         xmlns="http://www.w3.org/2000/svg"
-         className="absolute"
-         viewBox={`0 0 ${dim.x}} ${dim.y}}`}
-         width={0}
-         height={0}
-       >
-         <defs>
-   
-           <motion.clipPath
-             id={`detta`}
-             transform={`scale(${(dim.x ) / 100}, ${(dim.x ) / 100})`}
-           >
-             <motion.path
-              variants={pathVariant}
-              animate={variant}
-               // Start with the initial shape "M 3,3 0,12.5 V 50 87.5 L 8,100 H 50 87.5 92 l 8,-12.5 V 50 3 H 97 L 87.5,0 h -75 z"
-               d={"M 0,3 V 87.5 L 8,100 L 92,100 L 100,87.5 L 100,3 L 87.5,0 L 12.5,0 z"}
-             />
-           </motion.clipPath>
-   
-         </defs>
-       </svg>}
+      {dim &&
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute"
+          viewBox={`0 0 ${dim.x}} ${dim.y}}`}
+          width={0}
+          height={0}
+        >
+          <defs>
+
+            <motion.clipPath
+              id={`detta`}
+              transform={`scale(${(dim.x) / 100}, ${(dim.x) / 100})`}
+            >
+              <motion.path
+                variants={pathVariant}
+                animate={variant}
+                // Start with the initial shape "M 3,3 0,12.5 V 50 87.5 L 8,100 H 50 87.5 92 l 8,-12.5 V 50 3 H 97 L 87.5,0 h -75 z"
+                d={"M 0,3 V 87.5 L 8,100 L 92,100 L 100,87.5 L 100,3 L 87.5,0 L 12.5,0 z"}
+              />
+            </motion.clipPath>
+
+          </defs>
+        </svg>}
       <motion.div className=" ">
         <motion.div
           ref={test}
           className="relative w-96 h-96  "
           variants={maskerVariant}
           animate={variant}
-          onMouseEnter={() => setVariant("mouseEnter")}
-          onMouseLeave={() => setVariant("default")}
+          onMouseEnter={() => setVariant("default")}
+          onMouseLeave={() => setVariant("mouseEnter")}
         >
 
           <motion.div
@@ -189,12 +189,12 @@ const GeneralItem = () => {
 
           >
             <motion.div
-            className="absolute hidden bg-blue-700"
-            variants={frontVariant}
-            animate={variant}
+              className="absolute  bg-blue-700"
+              variants={frontVariant}
+              animate={variant}
 
-          >
-            {/* <motion.div
+            >
+              {/* <motion.div
               className="absolute"
               style={{
                 top: 25 + "%",
@@ -209,10 +209,10 @@ const GeneralItem = () => {
             >
               <ProjectCard project={testdata[1]} />
             </motion.div> */}
-          </motion.div>
+            </motion.div>
 
           </motion.div>
-          
+
         </motion.div>
       </motion.div>
       {/* <button
@@ -225,39 +225,98 @@ const GeneralItem = () => {
     </div>
   );
 };
+const d= `M 0,3 
+L 0,99              
+L 8,100 
+L 100,100 
+L 100,87.5 
+L 100,3 
+L 87.5,0 
+L 12.5,0 
+z`
 
+"m 0,5 v 80.5 a 6.8352055,6.8352055 73.690378 0 0 1.0781074,3.684543 l 5.8437852,9.130914 A 3.6545315,3.6545315 28.690378 0 0 10,100 h 80 a 3.6545315,3.6545315 151.30962 0 0 3.078107,-1.684543 l 5.843786,-9.130914 A 6.8352055,6.8352055 106.30962 0 0 100,85.5 V 5 A 2.5367936,2.5367936 51.747867 0 0 98.055225,2.5332541 L 89.444775,0.4667459 A 16.903307,16.903307 6.7478666 0 0 85.5,0 h -71 A 16.903307,16.903307 173.25213 0 0 10.555225,0.4667459 L 1.9447746,2.5332541 A 2.5367936,2.5367936 128.25213 0 0 0,5 Z"
+/**
+ * 
+ */
 const pathVariant: Variants = {
   default: {
-    d: [`M 0,3 L 0,100 L 8,100 L 100,100 L 100,87.5 L 100,3 L 87.5,0 L 12.5,0 z`]
-
+    d: `M 1.5,2.5
+        C 0,3  0,4  0,6
+        L 0,98
+        C 0,99   1,100  2,100 
+        L 7,100
+        C 7,100   8,100   9,100 
+        L 98,100
+        C 99,100   100,99   100,98      
+        L 100,87.5 
+        C 100,87.5   100,87.5   100,87.5
+        L 100,6
+        C 100,4   100,3   98.5,2.5
+        L 88.5,0.25
+        C 88,0.125   87.5,0   87,0  
+        L 13.5, 0
+        C 13,0   12.5,0.125   12,0.25
+        z`,
+    transition:{
+      duration: 1
+    }
   },
 
   mouseEnter: {
-    d:[`M 0,3 L 0,100 L 8,100 L 100,100 L 100,87.5 L 100,3 L 87.5,0 L 12.5,0 z`,
-      `M 0,12.5 L 0,100 L 8,100 L 87.5,100 L 100,87.5 L 100,3 L 87.5,0 L 12.5,0 z`
-    ]
+    d: 
+    `M 0.5,11.5 
+      C 0.125,12   0,12.5   0,13
+      L 0,98 
+      C 0.125,99   0.5,99.5   2,100
+      L 7,100 
+      C 8,100   9,100   10,100
+      L 86,100
+      C 87,100   88,99   89,98 
+      L 99.5,88
+      C 100,87.5   100,87   100,86.5
+      L 100,1 
+      C 100,0.5   99.5,0   99,0 
+      L 87.5,0
+      C 87.5,0   87.5,0   87.5,0 
+      L 17,0 
+      C 16.2,0.125   15.8,0.5   15,1 
+      z`
+    ,
+   
+    transition:{
+      duration: 1
+    }
   },
 };
 const maskerVariant: Variants = {
   default: {
     top: 0 + "%",
     left: 0 + "%",
-    bottom: 0+ "%",
+    bottom: 0 + "%",
     right: 0 + "%",
-
+    rotateX: 0,
+    rotateY: 0,
+    scale: 1, 
+    transition:{
+      duration: 1.5
+    }
   },
 
   mouseEnter: {
     rotateX: 20,
     rotateY: 55,
-    scale: 1
+    scale: 1, 
+    transition:{
+      duration: 1.5
+    }
   },
 };
 
 const shapeBoxVariant: Variants = {
   default: {
     top: 0 + "%",
-    left:  0+ "%",
+    left: 0 + "%",
     bottom: 0 + "%",
     right: -10 + "%",
   },
@@ -272,17 +331,32 @@ const shapeBoxVariant: Variants = {
 };
 const frontVariant: Variants = {
   default: {
+    
+    borderTopLeftRadius: 1 + "%",
+    borderTopRightRadius: 1 + "%",
+    borderBottomRightRadius: 1 + "%",
+
     top: 3 + "%",
     left: 0 + "%",
     bottom: 0 + "%",
-    right: 5 + "%",
+    right: 0 + "%",
+    
+    transition:{
+      duration: 2
+    }
+
   },
 
   mouseEnter: {
+    borderBottomRightRadius: 1 + "%",
+    borderTopRightRadius: 1 + "%",
     top: 12 + "%",
-    left: 12 + "%",
+    left: 0 + "%",
     bottom: 0 + "%",
-    right: 9 + "%",
+    right: 12.8 + "%",
+    transition:{
+      duration: 2
+    }
   },
 };
 
