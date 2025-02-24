e3640b77dfe0fc58ffb00ea3482696ca5fae4d45
Linus L <linlo001@student.liu.se>
2025-02-24 21:07:51 +0100

    cleanup for display

--- frontend/myportfolio/app/components/sandboxtesting/threeFiberDreiRapier/Cursor.tsx
+++ frontend/myportfolio/app/components/sandboxtesting/threeFiberDreiRapier/Cursor.tsx
@@ -3,10 +3,10 @@ export default function Cursor(props : any) {
     return (
         <group dispose={null} {...props}>
         <group rotation={[0, Math.PI / 1.8, -0.3]} scale={0.5}>
-          {/* Replace the original meshes with a sphere */}
+          
           <mesh>
-            <sphereGeometry args={[0.1, 16, 16]} /> {/* Small sphere with radius 0.1 */}
-            <meshStandardMaterial color="white" /> {/* White material */}
+            <sphereGeometry args={[0.1, 16, 16]} /> 
+            <meshStandardMaterial color="white" /> 
           </mesh>
         </group>
       </group>
